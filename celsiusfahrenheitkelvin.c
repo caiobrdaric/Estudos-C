71323843035beeb54b57731b3151fabdca7ad04c
Caio Brdaric <caiobrdaric@gmail.com>
2022-04-07 14:23:16 -0300

    Ajuste de variáveis

--- celsiusfahrenheitkelvin.c
+++ celsiusfahrenheitkelvin.c
@@ -2,14 +2,14 @@
 
 int main(){
 
-    float celcius, fahrenheit;
-    float convercao1, convercao2;
+    float celcius;
+    float fahrenheit, kelvin;
 
     printf ("Digite o valor em celcius:");
     scanf ("%f", &celcius);
-    convercao1 = celcius*1.8+32;
-    convercao2 = celcius+273;
-    printf ("Sua temperatura em fahrenheit é: %.0f°F \n", convercao1);
-    printf ("E em kelvin é: %.0f°K", convercao2);
+    fahrenheit = celcius*1.8+32;
+    kelvin = celcius+273;
+    printf ("Sua temperatura em Fahrenheit é: %.0f°F \n", fahrenheit);
+    printf ("E em Kelvin é: %.0f°K", kelvin);
     return 0;
 }
