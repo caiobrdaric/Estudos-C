c2a69396da1986478c7d601c56dd0de48746e04c
Caio Brdaric <caiobrdaric@gmail.com>
2022-04-09 23:58:27 -0300

    Alterações

---
+++ index.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+ 
+    int estacao, meses;
+    printf("Digite o mes numericamente:");
+    scanf ("%d" meses);
+
+    switch (estacao)
+    case '1'
+    case '2'
+    case '3': {
+        printf
+    }
+    return 0;
+}
