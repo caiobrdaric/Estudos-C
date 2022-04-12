e3e4b0deb5251f43a8c51359b84ef2c1b8b9f36f
Caio Brdaric <caiobrdaric@gmail.com>
2022-04-11 23:55:56 -0300

    Mudança de estrutura

--- estacoesDoAno.c
+++ estacoesDoAno.c
@@ -4,41 +4,42 @@
 
 int main (){
  
-    int estacao, meses;
-    printf("Digite o mes que deseja saber a estacao (numericamente):");
-    scanf ("%d" meses);
+    int estacao;
+    printf("Digite numericamente o mes que deseja saber a estacao:");
+    scanf ("%d", &estacao);
+    
+    printf("A estacao e:", estacao);
 
-    switch (estacao){
-    case '1'
-    case '2'
-    case '3': {
+    switch (estacao)
+    {
+    case '1':
+    case '2':
+    case '3': 
         printf("Verao");
-    }
-    break;
-    case '4'
-    case '5'
-    case '6': {
+        break;
+
+    case '4':
+    case '5':
+    case '6': 
         printf("Outono");
-    }
     break;
-    case '7'
-    case '8'
+
+    case '7':
+    case '8':
     case '9': 
-    {
         printf("Inverno");
-    } 
     break;
-    case '10'
-    case '11'
+
+    case '10':
+    case '11':
     case '12': 
-    {
         printf("Primavera");
-    }
-    default 
-    {
-        printf("Invalido");
-    }
-    return 1;
+    break;
+    
+    default:
+      printf("Invalido");
+
+     return 1;
     } 
     return 0;
 }
