e3b42a19f72fbfa5c7509bc7df069b29a330c2d1
Caio Brdaric <caiobrdaric@gmail.com>
2022-04-18 23:55:08 -0300

    Esboço

---
+++ calculoSalarioLiquido.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib>
+#include <math.h>
+
+int main()
+{
+float salario, aliquota, salLiq;
+
+printf("Digite o salário do funcionario");
+scanf("%f" &salario);
+
+printf("Salário liquido de %.2f:", salLiq);
+
+if (salario <= 1903.38)
+{
+    //Insento de alíquota
+}
+
+if (salario >= 1903.39 && salario <= 2826.65)
+{
+    //Aplicar 7,5% de alíquota
+}
+
+if (salario >= 2826.66 && salario 3751.06 <=)
+{
+    //Aplicar 15% de alíquota
+}
+
+if (salario >= 3751.07 && salario 4664.68 <=)
+{
+    //Aplicar 22,5% de alíquota
+}
+
+if (salario > 4664.69)
+{
+    //Aplicar 27,5% de alíquota
+}
+    return 0;
+}
