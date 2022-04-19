381c3c01cf15f331bb84a13e57d1f75d776c03ee
Caio Brdaric <97686912+caiobrdaric@users.noreply.github.com>
2022-04-19 18:04:58 -0300

    Update calculoSalarioLiquido.c

--- calculoSalarioLiquido.c
+++ calculoSalarioLiquido.c
@@ -7,28 +7,28 @@ int main()
         scanf("%f", &salario);
     if (salario <= 1903.38)
     {
-        printf("Não há alicota");
+    salLiq=salario;
     }
     if (salario >= 1903.39 && salario <= 2826.65)
     {
         aliquota=0.925;
-        salLiq=salario*aliquota;
+        salLiq=(salario*aliquota)-142.80;
     }
     if (salario >= 2826.66 && salario <= 3751.06)
     {
         aliquota=0.85;
-        salLiq=salario*aliquota;
+        salLiq=(salario*aliquota)-354.80;
     }
     if (salario >= 3751.07 && salario <= 4664.68)
     {
         aliquota=0.775;
-        salLiq=salario*aliquota;
+        salLiq=(salario*aliquota)-636.13;
     }
     if (salario >= 4664.69)
     {
         aliquota=0.725;
-        salLiq=salario*aliquota;
+        salLiq=(salario*aliquota)-869.36;
     }
-    printf("Salário liquido de %.3f:", salLiq);
+    printf("Salário liquido de %.2f:", salLiq);
     return 0;
-} 
+}
