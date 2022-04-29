cd59398842c499157a0db144707a0c9b19ce01bf
Caio Brdaric <caiobrdaric@gmail.com>
2022-04-28 23:59:17 -0300

    Algoritimo que emite numeros primos, sem limite adicionado

---
+++ numerosPrimos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero, quociente, primo, maximo;
+    printf("Digite o valor maximo:");
+    scanf("%d", &maximo);
+    
+    for (numero = 2; numero <=maximo, numero++;)
+    {
+        primo = 1;
+        for (quociente = 2; quociente<numero; quociente++)
+        {
+            if (numero % quociente == 0)
+            {
+                primo = 0;
+            }
+        }
+        if (primo)
+        {
+            printf("/t %d", numero);
+        }
+    }
+    return 0;
+}
